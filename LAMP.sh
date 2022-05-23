107ae7bcd574f1b99944ba9097706a85f8e928a9
Abdallah Muftah <scar.naruto@hotmail.com>
2022-05-23 19:38:30 +0400

    Update LAMP.sh

--- LAMP.sh
+++ LAMP.sh
@@ -70,12 +70,12 @@ systemctl stop mariadb.service
 systemctl start mariadb.service
 systemctl enable mariadb.service
 #
-#mysql_secure_installation
-wget https://raw.githubusercontent.com/abdomuftah/LAMP-Plus/main/assets/mysql_secure_installation.sh
-sed -i "s/complex_password/$sqpass/g" /root/mysql_secure_installation.sh
-chmod +x mysql_secure_installation.sh
-./mysql_secure_installation.sh
-rm mysql_secure_installation.sh
+mysql_secure_installation
+#wget https://raw.githubusercontent.com/abdomuftah/LAMP-Plus/main/assets/mysql_secure_installation.sh
+#sed -i "s/complex_password/$sqpass/g" /root/mysql_secure_installation.sh
+#chmod +x mysql_secure_installation.sh
+#./mysql_secure_installation.sh
+#rm mysql_secure_installation.sh
 #
 systemctl restart mysql.service
 #
@@ -125,7 +125,7 @@ echo "=================================="
 echo "Fixing MySQL And phpMyAdmin"
 echo "=================================="
 wget https://raw.githubusercontent.com/abdomuftah/LAMP-Plus/main/assets/fix.sql
-mysql -u root < fix.sql 
+#mysql -u root < fix.sql 
 service mysql restart
 systemctl restart apache2.service
 rm fix.sql 
