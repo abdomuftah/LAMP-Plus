6f54ef254d43b253f7ffdf83a1ac7149e729b3c9
Abdallah Muftah <scar.naruto@hotmail.com>
2022-05-23 23:22:25 +0400

    Update LAMP.sh

--- LAMP.sh
+++ LAMP.sh
@@ -79,7 +79,7 @@ systemctl restart mysql.service
 echo "=================================="
 echo "installing PHP 8.1 + modules"
 echo "=================================="
-apt -y install php8.1 php8.1-curl php8.1-common php8.1-cli php8.1-mysql php8.1-sqlite3 php8.1-intl php8.1-gd php8.1-mbstring php8.1-fpm php8.1-xml php8.1-zip php8.1-bcmath php8.1-sqlite3 php8.1-gd php8.1-intl php8.1-xmlrpc php8.1-soap php8.1-bz2 php8.1-imagick php8.1-tidy tar redis-server sed composer
+apt -y install php8.1 php8.1-curl php8.1-common php8.1-cli php8.1-mysql php8.1-sqlite3 php8.1-intl php8.1-gd php8.1-mbstring php8.1-fpm php8.1-xml php8.1-zip php8.1-bcmath libapache2-mod-php8.1 php8.1-sqlite3 php8.1-gd php8.1-intl php8.1-xmlrpc php8.1-soap php8.1-bz2 php8.1-imagick php8.1-tidy tar redis-server sed composer
 systemctl restart apache2.service
 #
 echo "=================================="
